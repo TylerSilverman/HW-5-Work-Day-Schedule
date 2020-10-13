f0a525684db0addc322552cfec010eba8f0cd682
Tyler Silverman <silverman.tyler@gmail.com>
2020-10-13 12:51:50 -0400

    added comments to go back to later

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Function for setting the time
+
 $(document).ready(function(){
     
 var timeNow = moment().format('LLLL');
@@ -7,7 +9,7 @@ $("#date").text(timeNow);
 var timeNow = moment().hour();
 
 })
-
+// function for the save button 
 
 $('.saveBtn').on('click', function() {
    console.log('save button clicked');
@@ -16,3 +18,14 @@ $('.saveBtn').on('click', function() {
    console.log(buttonClicked);
 
 });
+// funtion for storing the save information 
+
+// var information = localStorage.getItem("storedItem");
+
+// function buttonClicked(){
+
+//   var information = document.getElementById("input").value;
+
+//   localStorage.setItem("storedItem", information);
+//   document.getElementById("saveText").innerHTML = item + " save";
+// }
