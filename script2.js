212ccacf48066d883e8345f5c61fa6e70aa73c61
Tyler Silverman <silverman.tyler@gmail.com>
2020-10-15 14:23:42 -0400

    created another js tab to test out other functions

---
+++ script2.js
@@ -0,0 +1,48 @@
+// variable for hoursPerDay
+var hoursPerday = [
+    {
+       id: "hour9am",
+       textInformation: "",
+    },
+    {
+       id: "hour10am",
+       textInformation: "",
+       time: "10am",
+    },
+    {
+       id: "hour11am",
+       textInformation: "",
+       time: "11am",
+    },
+    {
+       id: "hour12pm",
+       textInformation: "",
+       time: "12pm",
+    },
+    {
+       id: "hour1pm",
+       textInformation: "",
+       time: "1pm",
+    },
+    {
+       id: "hour2pm",
+       textInformation: "",
+       time: "2pm",
+    },
+    {
+       id: "hour3pm",
+       textInformation: "",
+       time: "3pm",
+    },
+    {
+       id: "hour4pm",
+       textInformation: "",
+       time: "4pm",
+    },
+    {
+       id: "hour5pm",
+       textInformation: "",
+       time: "5pm",
+    },
+ ]
+
