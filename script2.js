283e2dbd07c6d4c3ccce094d19899767cf7f799d
Tyler Silverman <silverman.tyler@gmail.com>
2020-10-15 15:10:11 -0400

    added function to each id time when clicking submit it saved info to console.log

--- script2.js
+++ script2.js
@@ -65,6 +65,80 @@ $(document).ready(function(){
     });
 });
 
+// function for textbox info for each time to save when clicking save button 
+$(document).ready(function(){
+    $(".saveBtn").click(function(){
+        var textInformation = $.trim($("#textInformation9am").val());
+        if (textInformation !=""){
+            console.log(textInformation);
+        }
+    });
+});
+$(document).ready(function(){
+    $(".saveBtn").click(function(){
+        var textInformation = $.trim($("#textInformation10am").val());
+        if (textInformation !=""){
+            console.log(textInformation);
+        }
+    });
+});
+$(document).ready(function(){
+    $(".saveBtn").click(function(){
+        var textInformation = $.trim($("#textInformation11am").val());
+        if (textInformation !=""){
+            console.log(textInformation);
+        }
+    });
+});
+$(document).ready(function(){
+    $(".saveBtn").click(function(){
+        var textInformation = $.trim($("#textInformation12pm").val());
+        if (textInformation !=""){
+            console.log(textInformation);
+        }
+    });
+});
+$(document).ready(function(){
+    $(".saveBtn").click(function(){
+        var textInformation = $.trim($("#textInformation1pm").val());
+        if (textInformation !=""){
+            console.log(textInformation);
+        }
+    });
+});
+$(document).ready(function(){
+    $(".saveBtn").click(function(){
+        var textInformation = $.trim($("#textInformation2pm").val());
+        if (textInformation !=""){
+            console.log(textInformation);
+        }
+    });
+});
+$(document).ready(function(){
+    $(".saveBtn").click(function(){
+        var textInformation = $.trim($("#textInformation3pm").val());
+        if (textInformation !=""){
+            console.log(textInformation);
+        }
+    });
+});
+$(document).ready(function(){
+    $(".saveBtn").click(function(){
+        var textInformation = $.trim($("#textInformation4pm").val());
+        if (textInformation !=""){
+            console.log(textInformation);
+        }
+    });
+});
+$(document).ready(function(){
+    $(".saveBtn").click(function(){
+        var textInformation = $.trim($("#textInformation5pm").val());
+        if (textInformation !=""){
+            console.log(textInformation);
+        }
+    });
+});
+
 
 
 
